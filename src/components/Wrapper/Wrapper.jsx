8ae0e8150e413a4b73f1ee38ae0d359b0ef861ff
hasnatfahmidkhan <hasnatfahmidkhan@gmail.com>
2025-12-26 17:35:50 +0600

    added wrapper component for scrollto top when user change the route

---
+++ src/components/Wrapper/Wrapper.jsx
@@ -0,0 +1,17 @@
+import { useEffect } from "react";
+import { useLocation } from "react-router";
+
+const Wrapper = ({ children }) => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  }, [pathname]);
+
+  return children;
+};
+
+export default Wrapper;
--- src/layouts/root/Root.jsx
+++ src/layouts/root/Root.jsx
@@ -2,18 +2,19 @@ import { Outlet } from "react-router";
 import Footer from "../../pages/shared/Footer/Footer";
 import Container from "../../components/Container/Container";
 import Navbar from "../../pages/shared/Navbar/Navbar";
+import Wrapper from "../../components/Wrapper/Wrapper";
 
 const Root = () => {
   return (
     <div className="flex flex-col justify-center min-h-screen">
       {/* nav  */}
-
       <Navbar />
       <main className="flex-1">
         <Container>
           <Outlet />
         </Container>
       </main>
+      <Wrapper />
       {/* footer */}
       <Footer />
     </div>
